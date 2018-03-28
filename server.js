08123cb65adf7ca2d50d27d250982a7f9e0f0214
Vinicius Noda <noda.94@gmail.com>
2018-03-28 16:02:41 -0300

    new page

--- server.js
+++ server.js
@@ -50,6 +50,12 @@ app.get('/bad',(req,res)=>{
     });
 });
 
+app.get('/projects', (req,res)=>{
+    res.render('projects.hbs',{
+        pageTitle: 'Project page'
+    })
+});
+
 app.listen(port, () => {
     console.log(`Server is up on port ${port}`);
 });
